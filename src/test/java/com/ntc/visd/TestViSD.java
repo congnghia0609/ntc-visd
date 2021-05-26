aa8172ff0c2ea959cf9a5756fb04504f5f494917
nghiatc <congnghia0609@gmail.com>
2021-05-27 00:33:06 +0700

    add unit test

---
+++ src/test/java/com/ntc/visd/TestViSD.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2021 nghiatc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ntc.visd;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author nghiatc
+ * @since May 26, 2021
+ *
+ * cd ntc-visd
+ *
+ * # Run all the unit test classes. 
+ * mvn test
+ *
+ * # Run a single test class. 
+ * mvn -Dtest=TestApp1 test
+ *
+ * # Run a single test method from a test class. 
+ * mvn -Dtest=TestApp1#methodname test
+ */
+public class TestViSD {
+
+    private static ViSD visd;
+
+    @BeforeClass
+    public static void init() throws IOException {
+        visd = new ViSD();
+    }
+
+    @Before
+    public void beforeEachTest() {
+        //System.out.println("This is executed before each Test");
+    }
+
+    @After
+    public void afterEachTest() {
+        //System.out.println("This is executed after each Test");
+    }
+
+    @Test
+    public void testDefault() {
+        String s = "Đây là câu 1. Đây là câu 2. Đây là câu 3.";
+        // Sentence Detector
+        List<String> sents = visd.visd2List(s);
+        //System.out.println(sents);
+        Assert.assertEquals("Test Default", 3, sents.size());
+    }
+}
